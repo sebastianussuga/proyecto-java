5b7a6ab33ef8e990bc44d891044397c184fd81b8
sebastian <sebastianusugagarcia@gmail.com>
2024-11-05 20:39:38 -0500

    Corrección de errores en CalculadoraSimple

--- CalculadoraSimple.java
+++ CalculadoraSimple.java
@@ -1,11 +1,11 @@
 public class CalculadoraSimple {
 
     public int sumar(int a, int b) {
-        return a + b 
+        return a + b; // Añadir punto y coma al final de la línea
     }
 
     public int multiplicar(int a, int b) {
-        int resultado = a * b
+        int resultado = a * b; // Añadir punto y coma al final de la línea
         return resultado;
     }
 
@@ -15,7 +15,8 @@ public class CalculadoraSimple {
         int resultadoSuma = calc.sumar(10, 5);
         int resultadoMultiplicacion = calc.multiplicar(10, 10);
 
-        System.out.println(Resultado de la suma: " + resultadoSuma);
+        System.out.println("Resultado de la suma: " + resultadoSuma); // Corregir la cadena para la impresión
         System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
     }
 }
+
